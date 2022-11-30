701b797d1d6ba736e228c84a5d6d2eb7f57598ee
Mikhail Zharchev <mikhailzharchev@Mikhails-MacBook-Pro.local>
2022-11-30 22:44:02 +0400

    update

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-let draggableEl = document.querySelector(".draggable");
-
-draggableEl.addEventListener("click", () => {
-  console.log("clicked");
-});
+// let draggableEl = document.querySelector(".draggable");
+let draggableEls = document.querySelectorAll(".draggable");
 
 let initX,
   initY = 0;
@@ -39,40 +36,83 @@ const isTouchDevice = () => {
 isTouchDevice();
 
 // Start mouse down / touch start
-draggableEl.addEventListener(events[deviceType].down, (e) => {
-  e.preventDefault();
 
-  // init x/y
-  //   initX = isTouchDevice() ? e.clientX : e.touches[0].clientX;
-  //   initY = isTouchDevice() ? e.clientY : e.touches[0].clientY;
-  initX = e.clientX;
-  initY = e.clientY;
-
-  // start movement
-  moveEl = true;
+draggableEls.forEach((el) => {
+  el.addEventListener(events[deviceType].down, (e) => {
+    initX = e.clientX;
+    initY = e.clientY;
+    moveEl = true;
+  });
 });
+
+// draggableEl.addEventListener(events[deviceType].down, (e) => {
+//   e.preventDefault();
+//   initX = e.clientX;
+//   initY = e.clientY;
+//   moveEl = true;
+// });
 
 // Move
-draggableEl.addEventListener(events[deviceType].move, (e) => {
-  if (moveEl) {
-    e.preventDefault();
-    let newX = e.clientX;
-    let newY = e.clientY;
-    draggableEl.style.top = draggableEl.offsetTop - (initY - newY) + "px";
-    draggableEl.style.left = draggableEl.offsetLeft - (initX - newX) + "px";
-    initX = newX;
-    initY = newY;
-  }
+
+draggableEls.forEach((el) => {
+  el.addEventListener(events[deviceType].move, (e) => {
+    if (moveEl) {
+      e.preventDefault();
+      let newX = e.clientX;
+      let newY = e.clientY;
+      el.style.top = el.offsetTop - (initY - newY) + "px";
+      el.style.left = el.offsetLeft - (initX - newX) + "px";
+      initX = newX;
+      initY = newY;
+    }
+  });
 });
+
+// draggableEl.addEventListener(events[deviceType].move, (e) => {
+//   if (moveEl) {
+//     e.preventDefault();
+//     let newX = e.clientX;
+//     let newY = e.clientY;
+//     draggableEl.style.top = draggableEl.offsetTop - (initY - newY) + "px";
+//     draggableEl.style.left = draggableEl.offsetLeft - (initX - newX) + "px";
+//     initX = newX;
+//     initY = newY;
+//   }
+// });
 
 // Mouse up
 let stopMovement;
-draggableEl.addEventListener(events[deviceType].up, (stopMovement = e) => {
-  moveEl = false;
+
+draggableEls.forEach((el) => {
+  el.addEventListener(events[deviceType].up, (stopMovement = e) => {
+    moveEl = false;
+  });
 });
 
-draggableEl.addEventListener("mouseleave", stopMovement);
+// draggableEl.addEventListener(events[deviceType].up, (stopMovement = e) => {
+//   moveEl = false;
+// });
 
-draggableEl.addEventListener(events[deviceType].up, (e) => {
-  moveEl = false;
+draggableEls.forEach((el) => {
+  el.addEventListener("mouseleave", stopMovement);
+});
+
+// draggableEl.addEventListener("mouseleave", stopMovement);
+
+draggableEls.forEach((el) => {
+  el.addEventListener(events[deviceType].up, (e) => {
+    moveEl = false;
+  });
+});
+
+// draggableEl.addEventListener(events[deviceType].up, (e) => {
+//   moveEl = false;
+// });
+
+// Slider magic
+const container = document.querySelector(".container-theming");
+const blackZone = document.querySelector(".black-zone");
+
+document.querySelector(".slider").addEventListener("input", (e) => {
+  container.style.setProperty("--position", `${e.target.value}% `);
 });
